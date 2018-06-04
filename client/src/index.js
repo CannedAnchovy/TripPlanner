a89f7f9648ec4ab18bd5408ab93bb4697eb9e995
CannedAnchovy <Xavier412958@gmail.com>
2018-06-04 14:48:23 +0800

    Install redux, react-redux. Rearrange folder structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,21 @@
-/* eslint-env browser */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import TripPlannerApp from './components/js/TripPlannerApp';
 
+const emptyReducer = (action, state) => {
+  return state;
+};
 
+const store = createStore(emptyReducer);
 const App = () => (
-  <MuiThemeProvider>
-    <TripPlannerApp />
-  </MuiThemeProvider>
+  <Provider store={store}>
+    <MuiThemeProvider>
+      <TripPlannerApp />
+    </MuiThemeProvider>
+  </Provider>
 );
 
 
